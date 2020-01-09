98cdb11509638482722d55b23d7f72eb90acfe19
brunopenso <viperp@gmail.com>
2020-01-09 19:00:01 -0300

    interface fixes

--- src/Result.js
+++ src/Result.js
@@ -8,8 +8,8 @@ export function Result () {
   const [idToken, setIdToken] = useState('')
   const [accessToken, setAccessToken] = useState('')
   const [refreshToken, setRefreshToken] = useState('')
-  const [apiCallBody, setApiCallBody] = useState('click on the button above')
-  const [s3CallBody, setS3CallBody] = useState('click on the button above')
+  const [result, setResult] = useState('')
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     setIdToken(ls.get('idtoken'))
@@ -17,8 +17,12 @@ export function Result () {
     setRefreshToken(ls.get('refreshtoken'))
   }, [])
 
+  function cleanState () {
+    setResult('starting...')
+    setError(false)
+  }
+
   function getCredencials () {
-    setS3CallBody('clicked - wait - getting results')
 
     const url = 'cognito-idp.us-east-1.amazonaws.com/' + process.env.REACT_APP_AWS_USERPOOL_ID
     const logins = {}
@@ -35,13 +39,14 @@ export function Result () {
       if (!err) {
         getS3Data()
       } else {
-        console.log('returned with error') // <-- might get called if something is missing, anyways self-descriptive
-        console.log(err)
+        setResult(JSON.stringify(error))
+        setError(true)
       }
     })
   }
 
   function callS3 () {
+    cleanState()
     getCredencials()
   }
 
@@ -54,14 +59,17 @@ export function Result () {
     var bucket = new AWS.S3()
     bucket.listObjectsV2(params1, (err, data) => {
       if (!err) {
-        setS3CallBody('has ' + data.Contents.length + ' objects')
+        setResult('has ' + data.Contents.length + ' objects')
+        setError(false)
       } else {
-        setS3CallBody('error getting content: ' + err)
+        setResult(JSON.stringify(err))
+        setError(true)
       }
     })
   }
 
   function callApiGateway () {
+    cleanState()
     axios({
       method: 'get',
       url: process.env.REACT_APP_AWS_API_GATEWAY,
@@ -70,22 +78,24 @@ export function Result () {
       }
     })
       .then(response => {
-        console.log(response)
-        setApiCallBody(response.data.body)
+        setResult('' + response.data.body)
+        setError(false)
       })
-      .catch(err => console.log(err))
+      .catch(err => {
+        setResult(JSON.stringify(err))
+        setError(true)
+      })
   }
 
   return (
     <div>
-      <h2>This is result page</h2>
-      <div className='buttons'>
+      <h3>Click on the buttons bellow</h3>
+      <div>
         <button onClick={callApiGateway}>Click to call an API</button>
         <button onClick={callS3}>Click to call a S3 API</button>
       </div>
-      <div className='result'>
-        <p>Result from the button {apiCallBody}</p>
-        <p>Result from the button {s3CallBody}</p>
+      <div className={error ? 'error' : (result.length > 0 ? 'result' : 'hide')}>
+        Here is the result of the action: {result}
       </div>
       <br />
       <p className='token'><b>ID token:</b> {idToken}</p>
